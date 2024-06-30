325c2dedeaec14365ab4f116dc9832c85a887c08
Deivison de alcantara <deivison-alcantara@hotmail.com>
2024-06-30 00:43:24 -0300

    test Unprocessable

--- test/test_model.py
+++ test/test_model.py
@@ -1,12 +1,21 @@
 import unittest
 from model.pessoa import Pessoa
 from pydantic_core import ValidationError
-
+from model.UnprocessableError import UnprocessableError
 class MainTest(unittest.TestCase):
     def setUp(self):...
         #preencher com dados ou instancias que serão usadas para cada teste individualmente
         # print(' --> Novo teste -->')
     
+    def test_model_Pessoa_apelido_null(self):
+       v1 = {
+            "apelido" : None,
+            "nome" : "Ana Barbosa",
+            "nascimento" : "1985-01-23",
+            "stack" : None
+        }
+       self.assertRaises(UnprocessableError, Pessoa, **v1)
+
     def test_model_Pessoa_stack_com_inteiro(self):
         
         v1 = {
